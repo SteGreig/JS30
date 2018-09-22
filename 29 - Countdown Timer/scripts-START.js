dbe3aa11bc16a4871688660874b030e0da28858b
Stephen Greig <stephengreig@Stephens-MacBook-Pro.local>
2018-09-22 22:14:40 +0100

    FINISHED final tool - whack-a-mole

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -2,19 +2,22 @@ let countdown;
 const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+	// clear any existing timers
 	clearInterval(countdown);
+
 	const now = Date.now();
 	const then = now + seconds * 1000;
-	console.log(now, then);
 	displayTimeLeft(seconds);
 	displayEndTime(then);
 
 	countdown = setInterval(() => {
 		const secondsLeft = Math.round((then - Date.now()) / 1000);
+		// check if we should stop it!
 		if(secondsLeft <= 0) {
 			clearInterval(countdown);
 			return;
 		}
+		// display it
 		displayTimeLeft(secondsLeft);
 	}, 1000);
 }
